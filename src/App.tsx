18e22584cacc57fa0af635273ad48180a818fa0a
JeffersonD4vid <Jeffersonmd2002@gmail.com>
2024-01-28 15:29:26 -0500

    Agregando

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { EjercicioUno } from "./typeScript/EjercicioUno";
+
+const App = () => {
+  return(
+    <div>
+      <h1 className="mt-4">Deber-Ejercicios</h1>
+      <EjercicioUno/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/typeScript/EjercicioUno.tsx
@@ -0,0 +1,12 @@
+export const EjercicioUno = () => {
+
+    
+
+
+
+    return(
+        <div>
+            <h3 className="mt-3">Ejercicios 1</h3>
+        </div>
+    )
+}
